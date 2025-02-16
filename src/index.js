cd776d1173afe149c23a964a14f66f5933fbdba5
Bhavin <patelbc94@gmail.com>
2025-02-16 22:45:39 +0530

    try to make database connnection from multiple way also used .env file

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/Connection.js";
+
+connectDB();
+dotenv.config({ path: "./env" });
+
+//==================================================================================================================================================
+/*import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("Error", (error) => {
+      console.error("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error", error);
+  }
+})();
+ */
+// Here we are Using IIFE to connect to the database and start the server.
+// We are using the constants.js file to get the database name and the port number. We are using the process.env to get the environment variables.
+// We are using the mongoose library to connect to the MongoDB database. We are using the express library to create the server. We are using the app.listen method to start the server.
+// We are using the app.on method to handle the error event. We are using the console.log method to log the server is running message. We are using the console.error method to log the error message.
